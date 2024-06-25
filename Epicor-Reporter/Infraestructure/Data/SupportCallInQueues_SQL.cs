9c5b809bc4a406d8c73a2343c8b2ce51b0c5888c
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2024-06-25 10:15:54 -0600

    01- Inicio del Proyecto

---
+++ Epicor-Reporter/Infraestructure/Data/SupportCallInQueues_SQL.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+namespace Infraestructure.Data
+{
+    public static  class SupportCallInQueues_SQL
+    {
+       private static string query = string.Empty;
+
+
+        public static string Query()
+        {
+            return query;
+        }
+    }
+}
